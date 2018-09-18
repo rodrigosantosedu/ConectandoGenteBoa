0cf23ec71239c548d15e812e2f2233789d796063
Rodrigo Santos <rodrigobsi@usp.br>
2018-09-17 22:39:06 -0300

    Protegi as rotas.Inseri no DB o id do user.Escondi o feed quando usuario não está logado.

--- collection/posts.js
+++ collection/posts.js
@@ -1,1 +1,10 @@
 Posts = new Mongo.Collection("posts");
+
+Meteor.methods({
+	"insertPost": function(textoDoFormulario){
+		Posts.insert({
+			texto: textoDoFormulario,
+			idDoAutor: Meteor.userId()
+		});
+	}
+});
