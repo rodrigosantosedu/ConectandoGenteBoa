afa93563cd45e37e94356713409039357b4cc751
Rodrigo Santos <rodrigobsi@usp.br>
2018-09-19 10:40:38 -0300

    Mostrando posts especificos do usuario em sua página de perfil

--- client/Perfil/Perfil.js
+++ client/Perfil/Perfil.js
@@ -2,8 +2,11 @@ Template.Perfil.helpers({
 	perfil: function(){
 		var idDoUsuario = FlowRouter.getParam("id");
 		var info = Meteor.users.findOne({_id: idDoUsuario});
-		console.log(info);
-
 		return info;
+	},
+	posts: function(){
+		var idDoUsuario = FlowRouter.getParam("id");
+		var postsDoPerfil = Posts.find({idDoAutor: idDoUsuario}).fetch();
+		return postsDoPerfil;
 	}
 });
