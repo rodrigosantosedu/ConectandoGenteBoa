cf280d642bb6d8a7d6ec6201acee4e884b1bd8eb
Rodrigo Santos <rodrigobsi@usp.br>
2018-09-19 10:15:36 -0300

    Pegando params pela url do perfil

---
+++ client/Perfil/Perfil.js
@@ -0,0 +1,7 @@
+Template.Perfil.helpers({
+	perfil: function(){
+		var idDoUsuario = FlowRouter.getParam("id");
+		var info = Meteor.users.findOne({_id: idDoUsuario});
+		console.log(info);
+	}
+});
