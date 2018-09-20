9a4cdd4b96d88670efa10cf83cc9bd3b063bb59d
Rodrigo Santos <rodrigobsi@usp.br>
2018-09-20 13:01:08 -0300

    Colocando botões de seguir e deixar de seguir.Criando evento para esses botões.

--- client/Perfil/Perfil.js
+++ client/Perfil/Perfil.js
@@ -9,4 +9,14 @@ Template.Perfil.helpers({
 		var postsDoPerfil = Posts.find({idDoAutor: idDoUsuario}).fetch();
 		return postsDoPerfil;
 	}
+
+});
+
+Template.Perfil.events({
+	"click .seguir": function(evento, template){
+		console.log("SEGUINDO");
+	},
+	"click .deixar-de-seguir": function(evento, template){
+		console.log("DES-SEGUINDO");
+	}
 });
