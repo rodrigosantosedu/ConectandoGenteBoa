5aade1e8d53f897f278e97f06c117fb8ff783b6f
Rodrigo Santos <rodrigobsi@usp.br>
2018-09-20 12:40:47 -0300

    Resolvendo bucha de postar posts vazios.

--- collection/posts.js
+++ collection/posts.js
@@ -2,7 +2,10 @@ Posts = new Mongo.Collection("posts");
 
 Meteor.methods({
     "inserirPost": function(textoDoFormulario) {
-        if(Meteor.userId() !== null) {
+        if(Meteor.userId() !== null && textoDoFormulario) {
+          // texto vazio é falso e undefined é falso e null é falso, sendo assim
+          //validamos o formulario de post para todos os casos que seriam vazios
+          //isso impede de postar um post vazio
             Posts.insert({
                 texto: textoDoFormulario,
                 idDoAutor: Meteor.userId(),
