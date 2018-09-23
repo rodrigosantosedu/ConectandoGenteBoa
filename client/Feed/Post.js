eace1aef2b5b45dac72597478cf0ea447210b683
Rodrigo Luiz <rodrigobsi@usp.br>
2018-09-23 00:06:29 -0300

    Ajustes nas informações de perfil.Usernames dos posts tem links que levam para seus perfils

--- client/Feed/Post.js
+++ client/Feed/Post.js
@@ -3,7 +3,7 @@ Template.Post.onCreated(function () {
 });
 
 Template.Post.helpers({
-	userNameDoAutor: function(){
+	usernameDoAutor: function(){
 		var idDoAutor = this.idDoAutor;
 		var autor = Meteor.users.findOne({_id: idDoAutor});
 		return autor.username;
